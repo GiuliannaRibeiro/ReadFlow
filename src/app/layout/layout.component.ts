25fc082de0ca1b2792f5b45c8d4b4e0fa0808358
Giulianna Ribeiro <giuliannaribeiro00@gmail.com>
2025-07-23 21:47:09 -0300

    feat: implement app-card components in layout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from "@angular/router";
+import { FooterComponent } from "./footer/footer.component";
+import { CardComponent } from '../components/card/card.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent, CardComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
