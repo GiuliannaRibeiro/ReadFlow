1a0faaf50a4b1b869c6d60d1e4f9597ee8794416
Giulianna Ribeiro <giuliannaribeiro00@gmail.com>
2025-07-28 22:32:15 -0300

    feat: add app-recommedantion in layout component

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from "@angular/router";
 import { FooterComponent } from "./footer/footer.component";
 import { CardComponent } from '../components/card/card.component';
+import { RecommendationComponent } from '../components/recommendation/recommendation.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet, FooterComponent, CardComponent],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent, CardComponent, RecommendationComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
