c0986ccf1640451bf4f2c0f7553eb099ce2a3bd1
Giulianna Ribeiro <giuliannaribeiro00@gmail.com>
2025-09-03 21:18:30 -0300

    refactor: migrate to loadComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,28 @@ import { LayoutComponent } from './layout/layout.component';
 export const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent,
-    children: []
+    loadComponent: () =>
+      import('./layout/layout.component').then(m => m.LayoutComponent),
+    children: [
+      { path: '', redirectTo: 'main', pathMatch: 'full' },
+
+      {
+        path: 'main',
+        loadComponent: () =>
+          import('./pages/main-cards/main-cards.component').then(
+            m => m.MainCardsComponent
+          )
+      },
+
+      // {
+      //   path: 'ratings',
+      //   loadComponent: () =>
+      //     import('./features/ratings/ratings.component').then(
+      //       m => m.RatingsComponent
+      //     )
+      // },
+
+      { path: '**', redirectTo: 'main' }
+    ]
   }
 ];
